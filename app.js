755a84f708c20b5eea16f5e1af35ddad18c1cb7a
Satyasri1995 <93003088+Satyasri1995@users.noreply.github.com>
2021-12-31 00:35:17 +0530

    chat app completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,8 +12,41 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use("/",(req,res,next)=>{
+app.use("/", (req, res, next) => {
     return res.render("chatPage")
+})
+
+io.on('connection', (socket) => {
+    console.log("client connected", socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("client disconnected", socket.id);
+    })
+
+    socket.on('leave-room', (room) => {
+        if (socket.rooms.has(room)) {
+            socket.to(room).emit("disconnected-from-room", `${socket.id} left the room ${room}`);
+            io.to(socket.id).emit('disconnected-from-room',`Disconnected from room ${room}`)
+            socket.leave(room);
+        }else{
+            io.to(socket.id).emit("disconnected-from-room", `Already Disconnecred from room ${room}`)
+        }
+
+    })
+
+    socket.on('message-to-server', (message) => {
+        if (message.room) {
+            socket.to(message.room).emit("message-to-client", { name: socket.id, msg: message.msg })
+        } else {
+            socket.broadcast.emit("message-to-client", { name: socket.id, msg: message.msg });
+        }
+    })
+
+    socket.on('connect-to-room', (room) => {
+        socket.join(room);
+        io.to(socket.id).emit('connected-to-room', `Connected to Room ${room}`);
+        socket.to(room).emit('connected-to-room', `${socket.id} joined in room ${room}`)
+    })
 })
 
 
@@ -20,8 +54,6 @@ app.use("/",(req,res,next)=>{
 
 
 
-
-
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('Server Started');
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,92 @@
-function sendMessage(){
-    const msg = document.querySelector("#input-text").value;
+const socket = io.connect();
+
+socket.on('message-to-client',(message)=>{
+    receiveMessage(message);
+})
+
+socket.on('connected-to-room',(room)=>{
+   const notification=`
+        <div class="line"></div>
+        <div class="info">${room}</div>
+        <div class="line"></div>`
+    const notifi=document.createElement('div');
+    notifi.setAttribute('class','notification connect d-flex flex-row align-items-center');
+    notifi.innerHTML=notification;
+    document.querySelector("#message-box").appendChild(notifi);
+})
+
+socket.on('disconnected-from-room',(room)=>{
+    const notification=`
+         <div class="line"></div>
+         <div class="info">${room}</div>
+         <div class="line"></div>`
+     const notifi=document.createElement('div');
+     notifi.setAttribute('class','notification disconnect d-flex flex-row align-items-center');
+     notifi.innerHTML=notification;
+     document.querySelector("#message-box").appendChild(notifi);
+ })
+
+window.onload = ()=>{
+    document.querySelector("#input-text").addEventListener('keyup',(event)=>{
+        if(event.keyCode==13){
+            sendMessage();
+        }
+        event.preventDefault();
+    })
+    document.querySelector("#input-text2").addEventListener('keyup',(event)=>{
+        if(event.keyCode==13){
+            joinRoom();
+        }
+        event.preventDefault();
+    })
+}
+
+function joinRoom(){
+    const room = document.querySelector("#input-text2").value;
+    socket.emit('connect-to-room',room)
+}
+
+function leaveRoom(){
+    const room = document.querySelector("#input-text2").value;
+    if(room){
+        socket.emit('leave-room',room);
+    }
+    document.querySelector("#input-text2").value="";
+}
+
+
+
+function receiveMessage(data){
     const baloon = document.createElement("div");
-    baloon.setAttribute("class","baloon sent d-flex flex-column");
+    baloon.setAttribute("class", "baloon receive d-flex flex-column");
     const name = document.createElement('span');
     name.classList.add("name");
-    name.innerText="Sender";
+    name.innerText = data.name;
     const message = document.createElement("span");
     message.classList.add("message");
-    message.innerText=msg;
+    message.innerText = data.msg;
     baloon.appendChild(name);
     baloon.appendChild(message);
     document.querySelector("#message-box").appendChild(baloon);
 }
+
+
+
+function sendMessage() {
+    const msg = document.querySelector("#input-text").value;
+    const baloon = document.createElement("div");
+    baloon.setAttribute("class", "baloon sent d-flex flex-column");
+    const name = document.createElement('span');
+    name.classList.add("name");
+    name.innerText = socket.id;
+    const message = document.createElement("span");
+    message.classList.add("message");
+    message.innerText = msg;
+    baloon.appendChild(name);
+    baloon.appendChild(message);
+    document.querySelector("#message-box").appendChild(baloon);
+    const room = document.querySelector("#input-text2").value;
+    socket.emit("message-to-server",{msg:msg,room:room});
+    document.querySelector("#input-text").value="";
+}
+
